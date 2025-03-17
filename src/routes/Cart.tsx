1a5d4a05c6dc9b6b5bf7fcf4cf277414d421428c
uidata11 <chan7pp@naver.com>
2025-03-17 10:39:50 +0900

    init

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { CART } from "../contextApi";
 
 const Cart = () => {
+  const { cart } = CART.store();
+
+  const index = cart.findIndex(ci=>?ci.id=== item.id)
+  if(index<0  ){
+    cart.push({...index,quan:1 })
+
+  }else{
+    cart[index].quan+=1
+  }
   return <div>Cart</div>;
 };
 
